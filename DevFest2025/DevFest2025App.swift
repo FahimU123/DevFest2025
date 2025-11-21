292a27174f1f6882ceb657bacdad046a5d746448
FahimU313 <115724162+FahimU313@users.noreply.github.com>
2025-11-21 12:31:07 -0500

    Initial Commit

---
+++ DevFest2025/DevFest2025App.swift
@@ -0,0 +1,17 @@
+//
+//  DevFest2025App.swift
+//  DevFest2025
+//
+//  Created by Fahim Uddin on 11/21/25.
+//
+
+import SwiftUI
+
+@main
+struct DevFest2025App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
